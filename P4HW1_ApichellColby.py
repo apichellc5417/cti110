1d751f4107aa5e5f06b4f6ef35bb144e6eaaeb8f
apichellc5417 <113453325+apichellc5417@users.noreply.github.com>
2022-11-08 14:18:19 -0500

    Update P4HW1_ApichellColby.py
    Final Version for submission

--- P4HW1_ApichellColby.py
+++ P4HW1_ApichellColby.py
@@ -1,4 +1,4 @@
-#### STILL IN PROGRESS ####
+#### FINISHED ####
 
 
 # CTI-110
@@ -6,15 +6,21 @@
 # Colby Apichell 
 # 11/08/2022
 #
+# This program allows the user to enter multiple scores and then drops the lowest score.
+# The program then averages the scores and outputs rlevant data in the box below.
 
 
-#create a blank list
+# create a blank list
 
-#ask for number of scores
+# ask for number of scores
 
-#create loop for score input
+# input scores
 
-  #is score valid
+# is score valid?
+
+# do some math, dropping lowest score and averaging
+
+# show grade and relevant data
 
 
 scores = []
@@ -46,15 +52,6 @@ sum_of_grades = sum(scores)
 avg = (sum_of_grades / len(scores))
 
 
-### remove below line before submission
-##print('before', scores)
-
-
-
-
-### remove below line before submission
-##print('after', scores)
-
 if avg >= 90:
     final_grade = 'A'
 elif avg >= 80:
@@ -75,11 +72,3 @@ print('Modified List  :', scores)
 print('Scores Average : {:0,.2f}'.format(avg))
 print('Grade          :',final_grade)                      
 print('---------------------------------------')
-
-
-        
-
-
-        
-
-
