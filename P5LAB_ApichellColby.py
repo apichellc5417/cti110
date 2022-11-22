9464ecc925f55b3c3ccfbe1eadc921a5d2e779ca
apichellc5417 <113453325+apichellc5417@users.noreply.github.com>
2022-11-22 09:08:27 -0500

    Add files via upload

---
+++ P5LAB_ApichellColby.py
@@ -0,0 +1,17 @@
+def days_in_feb(user_year):  
+    if user_year % 4 == 0:
+        if user_year % 100 == 0:
+            if user_year % 400 == 0:
+                return 29
+            else:
+                return 28
+        else:
+            return 29
+    else:
+        return 28
+        
+
+if __name__ == '__main__':
+    year = int(input())
+    day = days_in_feb(int(year))
+    print(f'{year} has {day} days in February.')
